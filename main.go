ba16fd42389db19bd963ab7e2a400feca5ec1aa9
Yash Rohatgi <rohatgiy@gmail.com>
2024-08-22 21:58:58 -0400

    add skeleton for shorten URL flow

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"io"
+
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
-func initRouter() *gin.Engine {
+type ShortenURLRequestBody struct {
+	url string `json:"url"`
+}
+
+func initRouter(redisClient *redis.Client) *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
@@ -13,11 +20,44 @@ func initRouter() *gin.Engine {
 		})
 	})
 
+	router.POST("/mini", func(c *gin.Context) {
+		var body ShortenURLRequestBody
+		err := c.BindJSON(&body)
+
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Invalid request body",
+			})
+			return
+		}
+
+		// validate URL
+
+		// shorten URL
+
+		// save to Redis
+
+		// if redis is full, save to pgsql
+
+		// return shortened URL
+	})
+
 	return router
 }
 
+func initRedis() *redis.Client {
+	redis := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return redis
+}
+
 func main() {
-	router := initRouter()
+	redis := initRedis()
+	router := initRouter(redis)
 
 	router.Run()
 }
