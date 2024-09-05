2733e1f30a5bb69d6692f828e887c4c7c72d16ef
Yash Rohatgi <rohatgiy@gmail.com>
2024-09-05 11:44:09 -0400

    add .env

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load .env file: %s\n", err.Error())
+		os.Exit(1)
+		return
+	}
 	redis := initRedis()
 	postgres := initPostgres()
 	router := initRouter(redis, postgres)
